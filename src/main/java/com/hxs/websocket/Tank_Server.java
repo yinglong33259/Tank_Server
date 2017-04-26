7cbb34a41a734d0c0cd684d644177f83c80b38ad
xiaoshan <yonglong33259@gmail.com>
2017-04-26 20:40:59 +0800

    first version

---
+++ src/main/java/com/hxs/websocket/Tank_Server.java
@@ -0,0 +1,27 @@
+package com.hxs.websocket;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * Created by xiaoshan on 2017/4/25.
+ */
+@ServerEndpoint(value = "/server")
+public class Tank_Server {
+    @OnMessage
+    public void onMessage(String message,Session session){
+
+    }
+    @OnOpen
+    public void onOpen(Session session, EndpointConfig config){
+        System.out.println("liangjiechngogn");
+    }
+    @OnClose
+    public void onClose(Session session, CloseReason reason){
+
+    }
+    @OnError
+    public void onError(Session session, Throwable throwable){
+
+    }
+}
