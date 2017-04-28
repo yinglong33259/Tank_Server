d028a94aacc403fcb903d2f337ad5d8027fec0a4
xiaoshan <yonglong33259@gmail.com>
2017-04-28 16:40:21 +0800

    noting

--- src/main/java/com/hxs/websocket/DataManager.java
+++ src/main/java/com/hxs/websocket/DataManager.java
@@ -2,6 +2,8 @@ package com.hxs.websocket;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedList;
 
 import javax.websocket.Session;
 
@@ -12,8 +14,56 @@ public class DataManager {
 	public static final DataManager dataManager=new DataManager();
 	
 	public int playerNum=0;
-	public HashMap<String, Session> sessions;
-
+	//房间链表
+	public LinkedList<Integer> roomList=new LinkedList<Integer>();
+	//房间大小
+	public Hashtable<Integer,Integer> roomSize=new Hashtable<Integer,Integer>();
+	//
+	public Hashtable<String, Session> sessions=new Hashtable<String, Session>();
+	//保存用户在哪房间
+	public Hashtable<String,Integer> SID_Room=new Hashtable<String, Integer>();
+	//房间信息,前为房间编号
+	public Hashtable<Integer,ArrayList<Session>> Rooms=new Hashtable<Integer,ArrayList<Session>>();
+	
+	public void onMessage(Session session){
+			
+		}
+	
+	public void onOpen(Session session){
+		//房间总人数++
+		playerNum++;
+		//
+		sessions.put(session.getId(), session);
+		//如果是第一个人
+		if(roomList.size()==0){
+			roomList.add(1);
+			roomSize.put(1, 0);
+			Rooms.put(1, new ArrayList<Session>());
+		}
+		//房间不够加房间
+	}
+	public void onClose(Session session){
+		playerNum--;
+		sessions.remove(session.getId());
+	}
+	public void onError(Session session){
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public int getPlayerNum() {
         return playerNum;
     }
@@ -22,11 +72,11 @@ public class DataManager {
         this.playerNum = playerNum;
     }
     
-    public HashMap<String, Session> getSessions() {
+    public Hashtable<String, Session> getSessions() {
 		return sessions;
 	}
 
-	public void setSessions(HashMap<String, Session> sessions) {
+	public void setSessions(Hashtable<String, Session> sessions) {
 		this.sessions = sessions;
 	}
 
--- src/main/java/com/hxs/websocket/Tank_Server.java
+++ src/main/java/com/hxs/websocket/Tank_Server.java
@@ -12,26 +12,18 @@ public class Tank_Server {
 	
     @OnMessage
     public void onMessage(String message,Session session){
-    	
+    	dataManager.onMessage(session);
     }
     @OnOpen
     public void onOpen(Session session, EndpointConfig config){
-    	//游戏总人数加1
-    	dataManager.playerNum++;
-    	//Sessions数组加入浏览器Session绘画对象
-    	dataManager.sessions.put(session.getId(), session);
-    	//
-    	System.out.println(dataManager.playerNum+"s飒飒飒飒飒飒撒");
+		dataManager.onOpen(session);
     }
     @OnClose
     public void onClose(Session session, CloseReason reason){
-    	//游戏总人数-1
-    	dataManager.playerNum--;
-    	//Sessions数组加入浏览器Session绘画对象Player
-    	System.out.println(dataManager.sessions.get(session.getId()));
+    	dataManager.onClose(session);
     }
     @OnError
     public void onError(Session session, Throwable throwable){
-    	
+    	dataManager.onError(session);
     }
 }
